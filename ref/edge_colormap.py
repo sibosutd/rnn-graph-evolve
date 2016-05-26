fcbcd41684122f84d5480bf84af7e06e169fceda
sibo <sibo_song@mymail.sutd.edu.sg>
2016-05-26 19:29:04 +0800

    add ref code

--- ref/edge_colormap.py
+++ ref/edge_colormap.py
@@ -9,10 +9,10 @@ You must have matplotlib>=87.7 for this to work.
 import matplotlib.pyplot as plt
 import networkx as nx
 
-G = nx.cycle_graph(20)
-pos = nx.spring_layout(G, iterations=200)
-# nx.draw(G)
-nx.draw(G, pos, edge_color='#A0CBE2', node_color=range(20),
-        node_size=800, cmap=plt.cm.Blues, with_labels=True)
+G = nx.star_graph(20)
+pos = nx.spring_layout(G)
+colors = range(20)
+nx.draw(G, pos, node_color='#A0CBE2', edge_color=colors, width=4,
+        edge_cmap=plt.cm.Blues, with_labels=False)
 # plt.savefig("edge_colormap.png")  # save as png
 plt.show()  # display
