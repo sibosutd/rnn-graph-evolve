9b327f4fba1ec1033cbef41b8a0cabedf48feb53
sibosutd <sibo_song@mymail.sutd.edu.sg>
2016-06-10 11:51:37 +0800

    add dynamic evaluation

--- src/predict.py
+++ src/predict.py
@@ -23,24 +23,36 @@ NUM_OF_NODE = 100
 HIDDEN_UNITS = 256
 NUM_LAYER = 2
 STOP = 150
+epochs = 10
+select_index = 0
 
 # read data
 sequences = np.load(glob.DATASET_PATH+TYPE+'.npy')
+adj_matrices = np.load(glob.DATASET_PATH+TYPE+'_adj.npy')
 
 # read model and weights
-with open(glob.MODEL_PATH+TYPE+'_'+str(HIDDEN_UNITS)+'_'
-          + str(NUM_LAYER)+'_model.json') as f:
-    model = model_from_json(f.read())
+model_file = '{}{}_{}_{}_model.json' \
+             .format(glob.MODEL_PATH, TYPE, HIDDEN_UNITS, NUM_LAYER)
+weights_file = '{}{}_{}_{}_weights.h5' \
+               .format(glob.MODEL_PATH, TYPE, HIDDEN_UNITS, NUM_LAYER)
 
-model.load_weights(glob.MODEL_PATH+TYPE+'_'+str(HIDDEN_UNITS)
-                   + '_' + str(NUM_LAYER)+'_weights.h5')
+with open(model_file) as f:
+    model = model_from_json(f.read())
+model.load_weights(weights_file)
 
 model.compile(optimizer='rmsprop', loss='mse')
+model.summary()
 
-train_data = sequences[:1, 0:n_t_train, :]
+# train rnn with given history
+train_data = sequences[select_index, 0:n_t_train, :]
 y = np.roll(train_data, -1, axis=1)
 
-model.fit(train_data, y, batch_size=64, nb_epoch=1)
+# for i in range(epochs):
+#     print 'Epoch {}/{}'.format(i, epochs)
+#     model.fit(train_data, y, nb_epoch=1, verbose=0, shuffle=False)
+#     model.reset_states()
+
+model.fit(train_data, y, nb_epoch=epochs, verbose=1)
 
 # init sequences ndarray, 3-d because of training data
 sentences = np.zeros((1, n_timestamps+1, NUM_OF_NODE))
@@ -61,6 +73,9 @@ for i in np.arange(1, n_t_train):
 for i in np.arange(n_t_train, n_timestamps):
     if i % 10 == 0:
         print 'predict the '+str(i)+' edges...'
+    # dynamic evaluation using stateful rnn
+    model.fit(sequences[:, :i+1, :], sentences[:, :i+2, :],
+              nb_epoch=1, verbose=0)
     probs = model.predict_proba(sentences, verbose=0)[:, i, :]
     probs = probs / np.sum(probs)
     # set temperature to control the trade-off
@@ -86,13 +101,17 @@ for i in np.arange(n_t_train, n_timestamps):
         graph_matrix[select_nodes[1], select_nodes[0]] = 1
 
 # construct graph
-g = nx.Graph(graph_matrix)
+g_1 = nx.Graph(adj_matrices[select_index, :, :])
+g_2 = nx.Graph(graph_matrix)
+
+print sorted(nx.degree(g_1).values(), reverse=True)
+print sorted(nx.degree(g_2).values(), reverse=True)
 
 # plot graph figure
-pos = nx.nx_pydot.graphviz_layout(g)
+# pos = nx.nx_pydot.graphviz_layout(g)
 # color = [10*p for p in vis_proba[0].tolist()]
 
-nx.draw(g, pos, node_size=180)
+# nx.draw(g, pos, node_size=180)
 # save as png
 # plt.savefig(glob.RESULT_PATH+TYPE+'_'+str(STOP)+'_prob.png')
-plt.show()  # display
+# plt.show()  # display
