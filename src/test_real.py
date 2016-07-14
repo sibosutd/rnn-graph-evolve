42c9fd7370069234af729a79e90297f8e7b9f701
sibo <sibo_song@mymail.sutd.edu.sg>
2016-07-14 15:09:26 +0800

    fix plotting issue

--- src/test_real.py
+++ src/test_real.py
@@ -9,17 +9,20 @@ import matplotlib.pyplot as plt
 import glob
 glob.set_dir()
 
-sequences = np.load(glob.DATASET_PATH+'data/real_seq_000.npy')
+sequences = np.load(glob.DATASET_PATH+'real_seq.npy')
 
-n_timestamps, n_node = sequences.shape
+select_index = 5
 
+n_samples, n_timestamps, n_node = sequences.shape
+sequence = sequences[select_index]
 matrices = np.zeros((n_node, n_node))
 
 D = nx.Graph(np.zeros((n_node, n_node)))
 for step in np.arange(n_timestamps):
-    timestamp = sequences[step, :]
+    timestamp = sequence[step, :]
     indices = timestamp.nonzero()
-    D.add_edges_from(tuple(indices))
+    if indices[0].shape[0]:
+        D.add_edges_from(tuple(indices))
 matrices[:, :] = nx.to_numpy_matrix(D)
 
 D = nx.Graph(matrices)
